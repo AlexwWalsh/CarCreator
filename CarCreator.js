5c22d74a43ea2ac38b9ceabb2b35fdbfa9c624b3
sharon <sharontheginger@me.com>
2017-12-07 16:12:47 -0800

    first commit

--- CarCreator.js
+++ CarCreator.js
@@ -4,39 +4,37 @@ class Car {
         this.modelYear = 2007
         this.lights = "off"
         this.turnSignal = "off"
+        this.speed = 0
     }
     powerLights(power) {
-        if (power == "off"){
-            
-
+        if (power === "off"){
+        this.lights = "off"
+        } else if (power === "on") {
+        this.lights = "on"
+        } else {
+            return "No lights present"
         }
     }
-    turnOff() {
-        this.lights = "off"
+    signal(direction) {
+        if (direction === "left"){
+        this.turnSignal = "left"
+    } else if (direction === "right") {
+        this.turnSignal = "right"
+        } else {
+            return "Turn signal off"
+        }
     }
-
+    acceleration(fast){
+        this.speed += fast
+            }
+    deceleration(slow){
+        if (this.speed > 0){
+        this.speed -= slow
+    } else if (this.speed <=0) {
+        return "Car is fully stopped!"
+        }
+    }
 }
-
-
-
-var myCar = new Car
-
-
-//Class Car is created and given the attribute of 4 wheels.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -48,6 +46,30 @@ class Tesla extends Car{
 }
 
 var myTesla = new Tesla
+
+
+
+
+
+//Class Car is created and given the attribute of 4 wheels.
+
+
+var myCar = new Car
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
